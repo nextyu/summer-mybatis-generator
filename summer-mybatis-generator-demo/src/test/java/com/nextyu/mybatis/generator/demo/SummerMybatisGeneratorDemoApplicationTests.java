b32edf761c730e6b2cb10ce092af38d4d27da98f
zhouyu <1467237221@qq.com>
2017-11-28 13:44:20 +0800

    update package name

--- summer-mybatis-generator-demo/src/test/java/com/nextyu/mybatis/generator/demo/SummerMybatisGeneratorDemoApplicationTests.java
+++ summer-mybatis-generator-demo/src/test/java/com/nextyu/mybatis/generator/demo/SummerMybatisGeneratorDemoApplicationTests.java
@@ -9,12 +9,12 @@ public class SummerMybatisGeneratorDemoApplicationTests {
     @Test
     public void testGenerate() throws Exception {
 
-        String basePackageName = "com.nextyu.demo";
+        String basePackageName = "com.nextyu.spring.transaction.demo";
 
         Generator.newGenerator()
                 .ip("127.0.0.1").port("3306").db("test")
                 .userName("root").password("123456").tableNames("user")
-                .entityPackageName(basePackageName + ".domain")
+                .entityPackageName(basePackageName + ".entity")
                 .voPackageName(basePackageName + ".vo")
                 .queryPackageName(basePackageName + ".query")
                 .controllerPackageName(basePackageName + ".web.controller")
